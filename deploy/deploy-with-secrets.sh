482b791ab8bace4f7b9a04efff2948ff5dfa018d
eivibro <eivbro@gmail.com>
2024-01-02 21:36:04 +0100

    Adding some more files for remote deploy

--- deploy/deploy-with-secrets.sh
+++ deploy/deploy-with-secrets.sh
@@ -11,6 +11,7 @@ trap cleanup EXIT
 
 # Create the directory where sshd expects to find the host keys
 install -d -m755 "$temp/root/.ssh"
+install -d -m755 "$temp/etc/secrets/initrd"
 
 # Decrypt your private key from the password store and copy it to the temporary directory
 cat /home/eivbro/.ssh/ssh-secret > "$temp/root/.ssh/ssh_host_ed25519_key"
@@ -23,4 +24,4 @@ chmod 600 "$temp/etc/secrets/initrd/ssh_host_ed25519_key"
 chmod 644 "$temp/etc/secrets/initrd/ssh_host_ed25519_key.pub"
 
 # Install NixOS to the host system with our secrets
-nix run github:nix-community/nixos-anywhere -- --disk-encryption-keys /tmp/secret.key /tmp/secret.key --extra-files "$temp" --flake '../.#auto' root@192.168.60.10 
+nix run github:nix-community/nixos-anywhere -- --disk-encryption-keys /tmp/secret.key /tmp/secret.key --extra-files "$temp" --flake '../.#auto' root@192.168.44.3
